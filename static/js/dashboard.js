fbf992f329f8c9d6d2732424e5788d135894e3aa
tomblobla <hotrongvinh1234@gmail.com>
2021-09-04 15:39:14 +0700

    rename again

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,27 +1,3 @@
-$(document).ready(()=>{
-  
-    $('#open-sidebar').click(()=>{
-       
-        // add class active on #sidebar
-        $('#sidebar').addClass('active');
-        
-        // show sidebar overlay
-        $('#sidebar-overlay').removeClass('d-none');
-      
-     });
-    
-    
-     $('#sidebar-overlay').click(function(){
-       
-        // add class active on #sidebar
-        $('#sidebar').removeClass('active');
-        
-        // show sidebar overlay
-        $(this).addClass('d-none');
-      
-     });
-    
-  });
 
 
 // vẽ sparklinedash
